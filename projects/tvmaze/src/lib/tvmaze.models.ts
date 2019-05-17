02bcdbbde74a41c651756d9faee296b254ab98ab
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-05-17 13:00:50 +0200

    added models

---
+++ projects/tvmaze/src/lib/tvmaze.models.ts
@@ -0,0 +1,63 @@
+interface RootObject {
+    id: number;
+    url: string;
+    name: string;
+    type: string;
+    language: string;
+    genres: string[];
+    status: string;
+    runtime: number;
+    premiered: string;
+    officialSite: string;
+    schedule: Schedule;
+    rating: Rating;
+    weight: number;
+    network: Network;
+    webChannel?: any;
+    externals: Externals;
+    image: Image;
+    summary: string;
+    updated: number;
+    _links: Links;
+  }
+
+  interface Links {
+    self: Self;
+    previousepisode: Self;
+  }
+
+  interface Self {
+    href: string;
+  }
+
+  interface Image {
+    medium: string;
+    original: string;
+  }
+
+  interface Externals {
+    tvrage: number;
+    thetvdb: number;
+    imdb: string;
+  }
+
+  interface Network {
+    id: number;
+    name: string;
+    country: Country;
+  }
+
+  interface Country {
+    name: string;
+    code: string;
+    timezone: string;
+  }
+
+  interface Rating {
+    average: number;
+  }
+
+  interface Schedule {
+    time: string;
+    days: string[];
+  }
