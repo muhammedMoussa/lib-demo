0a3e202bd89ec80b29fe2f16f4306ea44013d09e
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-05-17 14:09:56 +0200

    using lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
 
+import { TvmazeService } from 'tvmaze'
 @Component({
   selector: 'ld-root',
-  templateUrl: './app.component.html',
+  template: `
+    <tm-poster [showId]="336"></tm-poster>
+    <pre>{{ show$ | async | json}}</pre>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lib-demo';
+  show$: Observable<any>;
+
+  constructor(private tvmaze: TvmazeService) {
+    this.show$ = this.tvmaze.getShow(336);
+  }
+
 }
