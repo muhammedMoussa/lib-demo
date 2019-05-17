749b727801d81feb99df11a90f88a44b30b77d85
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-05-17 13:01:11 +0200

    added service

--- projects/tvmaze/src/lib/tvmaze.service.ts
+++ projects/tvmaze/src/lib/tvmaze.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class TvmazeService {
+  private readonly apiRoot = 'https://api.tvmaze.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getShow(id: number): Observable<any> {
+    const url = `${this.apiRoot}/shows/${id}`
+    return this.http.get<any>(url);
+  }
 }
